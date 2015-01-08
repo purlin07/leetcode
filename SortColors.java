1ec7b9dbbed10e3820902d5bebd0317f2d485c68
purlin07 <ghan@g.clemson.edu>
2015-01-08 00:19:41 -0500

    75

---
+++ SortColors.java
@@ -0,0 +1,39 @@
+class SortColors
+{
+	public static void main(String[] args){
+		int[] test = {1,0,2,1,0,2};	
+		print(sortColors(test));
+	}
+
+	public static int[] sortColors(int[] A){
+		int begin = 0;
+		int end = A.length - 1;
+
+		int i = 0;
+		int temp;
+		while(i <= end){
+			if(A[i] == 0){
+				temp = A[i];
+				A[i] = A[begin];
+				A[begin] = temp; 
+				++i;
+				++begin;
+			}else if(A[i] == 2){
+				temp = A[i];
+				A[i] = A[end];
+				A[end] = temp;
+				--end;
+			}else{
+				++i;
+			}
+		}
+		return A;
+	}
+
+	public static void print(int[] arr){
+		for(int i : arr){
+			System.out.print(i + " ");
+		}
+		System.out.print("\n");
+	}
+}
